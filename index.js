e6077a9d04160392216b6df215f9884389cea9c1
shamiul <anik.savar.bd@gmail.com>
2023-09-26 03:11:45 +0600

    Vercel settings updated

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
 
     const paymentCollection = client.db("EduMentor").collection("payments");
 
